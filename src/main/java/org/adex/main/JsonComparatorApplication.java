caa96265c29dcb16d97ba399e54c7c2247e44188
mdouhi <mdouhi@sqli.com>
2022-09-07 18:27:35 +0100

    [REWORK]

--- src/main/java/org/adex/main/JsonComparatorApplication.java
+++ src/main/java/org/adex/main/JsonComparatorApplication.java
@@ -15,22 +15,27 @@ public class JsonComparatorApplication {
         String sampleObj3 = "{\"1\": \"A\", \"2\":\"C\", \"3\":\"C\"}";
         String complexObj = "{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}, \"2\":[0,1,2]}";
 
-        JsonComparator jsonComparator = new JsonComparatorImpl();
-        System.out.println(jsonComparator.assertEquals(sampleObj,sampleObj1));
-        System.out.println(jsonComparator.assertEquals(sampleObj,sampleObj2));
-        System.out.println(jsonComparator.assertEquals(sampleObj,sampleObj3));
-
         String array = "[0, 1, 2]";
         String array1 = "[0, 1, 2]";
         String array2 = "[0, 1, 3]";
         String array3 = "[0, 1, 2, 3]";
+
+        JsonComparator jsonComparator = new JsonComparatorImpl();
+        System.out.println(jsonComparator.assertEquals(sampleObj,sampleObj1));
+        System.out.println(jsonComparator.assertEquals(sampleObj,sampleObj2));
+        System.out.println(jsonComparator.assertEquals(sampleObj,sampleObj3));
+        System.out.println(jsonComparator.assertEquals(sampleObj,array));
+
         System.out.println(jsonComparator.assertEquals(array,array1));
         System.out.println(jsonComparator.assertEquals(array,array2));
         System.out.println(jsonComparator.assertEquals(array,array3));
+        System.out.println(jsonComparator.assertEquals(array,sampleObj));
 
         System.out.println(jsonComparator.assertEquals("4", "4"));
         System.out.println(jsonComparator.assertEquals("4", "5"));
         System.out.println(jsonComparator.assertEquals("4", "{45"));
+        System.out.println(jsonComparator.assertEquals("4", array));
+        System.out.println(jsonComparator.assertEquals("4", sampleObj));
 
     }
 
--- src/main/java/org/adex/utilities/JsonUtils.java
+++ src/main/java/org/adex/utilities/JsonUtils.java
@@ -1,11 +1,5 @@
 package org.adex.utilities;
 
-import lombok.extern.slf4j.Slf4j;
-import org.adex.dtos.Node;
-import org.adex.services.JsonComparator;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.json.simple.parser.ContainerFactory;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
